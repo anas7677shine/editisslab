1421b9c0e8721a4646904a2f2ef0087fbb839fb8
anas7677shine <anas7677shine@gmail.com>
2021-07-29 12:34:42 +0530

    first commit

---
+++ first.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int a,b;
+int main(){
+        printf("Enter two number\n");
+        scanf("%d",&a);
+        scanf("%d",&b);
+        printf("Sum of Two number is %d\n",a+b);
+        return 0;
+}
+
